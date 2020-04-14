99717c752a22ed62d004d3546bf3ad7f11f21021
Manuel <manueldipilla3@gmail.com>
2020-04-14 18:15:50 +0200

    tabella user

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function user_info()
+        {
+            return $this->hasOne('App\User_info');
+        }
 }
