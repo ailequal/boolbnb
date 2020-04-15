9aa9815c14421b28613688ddadce8a3aa626ad04
ailequal <37016865+ailequal@users.noreply.github.com>
2020-04-15 09:38:40 +0200

    Creato seeder generico

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(UserInfosTableSeeder::class);
+        $this->call(FlatsTableSeeder::class);
     }
 }
