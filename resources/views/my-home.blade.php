a8ddae5aa1d8f1fef6f092fbe5dd8491931f05be
Tetyana <tsergata@gmail.com>
2020-04-15 17:45:14 +0200

    home completata + query

--- resources/views/my-home.blade.php
+++ resources/views/my-home.blade.php
@@ -1,5 +1,56 @@
 @extends('layouts.boolbnb')
  @section('main')
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+   <div class="container promo">
+     <h1>Gli appartamenti in evidenza</h1>
+     <div class="row">
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+
+       <a href="#">
+         <div class="box-flat">
+           <img src="{{asset('images/world.jpg')}}" alt="">
+           <h3>Titolo</h3>
+           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+         </div>
+       </a>
+
+     </div>
+
+    
+   </div>
  @endsection
