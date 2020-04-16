d681ed74d5d3e15ead1c6ff7542f9b8e694eecce
ailequal <37016865+ailequal@users.noreply.github.com>
2020-04-16 15:58:33 +0200

    Aggiunti bottoni login e home intelligenti

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -1,41 +1,55 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
-    <title></title>
-  </head>
-  <body>
-    <header class="container">
-      <nav>
-        <div class="nav-left">
-          <a href="#">Boolbnb</a>
-        </div>
-        <div class="nav-right">
-          <ul>
-            <li>
-              <input class="search-bar" type="text" name="" value="">
-              <button class="search" type="button" name="button">
-                <i class="fas fa-search"></i>
-                Cerca
-              </button>
-            </li>
-            <li>
-              <button class="" type="button" name="button">Accedi</button>
-            </li>
-            <li>
-              <button class="" type="button" name="button">Registrati</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
 
-      <div class="search-mobile">
-        <input class="search-bar" type="text" name="" value="">
-        <button class="search" type="button" name="button">Cerca</button>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css"
+    integrity="sha256-h20CPZ0QyXlBuAw7A+KluUYx/3pK+c7lYEpqLTlxjYQ=" crossorigin="anonymous" />
+  <title></title>
+</head>
+
+<body>
+  <header class="container">
+    <nav>
+      <div class="nav-left">
+        <a href="#">Boolbnb</a>
       </div>
+      <div class="nav-right">
+        <ul>
+          <li>
+            <input class="search-bar" type="text" name="" value="">
+            <button class="search" type="button" name="button">
+              <i class="fas fa-search"></i>
+              Cerca
+            </button>
+          </li>
+          @if (Route::has('login'))
+          @auth
+          <li>
+            <a class="btn btn-primary" href="{{ url('/home') }}">Home</a>
+          </li>
+          @else
+          <li>
+            <a class="btn btn-primary" href="{{ route('login') }}">Accedi</a>
+          </li>
+          @if (Route::has('register'))
+          <li>
+            <a class="btn btn-primary" href="{{ route('register') }}">Registrati</a>
+          </li>
+          @endif
+          @endauth
+      </div>
+      @endif
+      </ul>
+      </div>
+    </nav>
+
+    <div class="search-mobile">
+      <input class="search-bar" type="text" name="" value="">
+      <button class="search" type="button" name="button">Cerca</button>
+    </div>
 
 
-    </header>
+  </header>
