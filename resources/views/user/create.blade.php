a83200325887b59b3242ed033fb603b47684fc55
Stefano Riccio <riccio.stefano0@gmail.com>
2020-04-16 12:55:34 +0200

    fatto rename della create temporanea

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,68 @@
+@extends('layouts.boolbnb')
+ @section('main')
+   <form action="" method="POST" enctype='multipart/form-data'>
+     {{-- token generator --}}
+     @csrf
+     @method('POST')
+
+     <div class="form-group">
+       <label for="title">Titolo</label>
+       <input type="text" name="title" placeholder="Titolo">
+     </div>
+
+     <div class="form-group">
+       <label for="address">Indirizzo</label>
+       <input type="text" name="address" placeholder="Indirizzo">
+     </div>
+
+     <div class="form-group">
+       <label for="rooms">Numero stanza</label>
+       <input type="number" name="rooms" placeholder="Numero stanza">
+     </div>
+
+     <div class="form-group">
+       <label for="mq">Metri quadri</label>
+       <input type="text" name="mq" placeholder="Metri quadri">
+     </div>
+
+     <div class="form-group">
+       <label for="cover">Immagine copertina</label>
+       <input type="file" name='cover' accept='image/*'>
+     </div>
+
+     <div class="form-group">
+       <label for="guest">Numero ospiti</label>
+       <input type="number" name="guest" placeholder="Numero ospiti">
+     </div>
+
+     <div class="form-group">
+       <label for="description">Descrizione</label>
+       <input type="text" name="description" placeholder="Descrizione">
+     </div>
+
+     <div class="form-group">
+       <label for="price_day">Prezzo giornaliero</label>
+       <input type="number" name="price_day" placeholder="Prezzo giornaliero">
+     </div>
+
+     <div class="form-group">
+       <label for="bathrooms">Numero bagni</label>
+       <input type="number" name="bathrooms" placeholder="Numero bagni">
+     </div>
+
+     {{-- Lat e Long mancano --}}
+
+     <div class="form-group">
+       <label for="hidden">Mostra appartamento</label>
+       <input type="checkbox" name="hidden">
+     </div>
+
+     <div class="form-group">
+       <label for="beds">Numero letti</label>
+       <input type="number" name="beds" placeholder="Numero letti">
+     </div>
+
+
+     <input type="submit" value="Submit">
+   </form>
+  @endsection
