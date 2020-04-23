95e9b402b7a8dc6bd8dbbac6fc1318e07588b2fc
ailequal <37016865+ailequal@users.noreply.github.com>
2020-04-23 12:53:47 +0200

    Aggiunti input extra per ricerca (interfaccia)

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -3,15 +3,82 @@
 @section('main')
 
 <div class="entry">
-	<label for="text">Radius</label>
 
+<label for="text">Radius</label>
 <select id="radius">
 	<option value="10">10</option>
 	<option value="20" selected="20">20</option>
 	<option value="30">30</option>
 	<option value="40">40</option>
 	<option value="50">50</option>
-</select> 
+</select>
+
+<label for="text">Letti</label>
+<select id="beds">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="6">6</option>
+	<option value="7">7</option>
+	<option value="8">8</option>
+	<option value="9">9</option>
+	<option value="10">10</option>
+
+</select>
+
+<label for="text">Stanze</label>
+<select id="rooms">
+	<option value="1">1</option>
+	<option value="2">2</option>
+	<option value="3">3</option>
+	<option value="4">4</option>
+	<option value="5">5</option>
+	<option value="6">6</option>
+	<option value="7">7</option>
+	<option value="8">8</option>
+	<option value="9">9</option>
+    <option value="10">10</option>
+    <option value="11">11</option>
+    <option value="12">12</option>
+    <option value="13">13</option>
+    <option value="14">14</option>
+    <option value="15">15</option>
+
+</select>
+
+
+<div class="extra-services">
+	<ul>
+		<li>
+			<label for="wifi">Wi Fi</label>
+			<input type="checkbox" name="wifi">
+		</li>
+		<li>
+			<label for="smoking">Area fumatori</label>
+			<input type="checkbox" name="smoking">
+		</li>
+		<li>
+			<label for="parking">Parcheggio</label>
+			<input type="checkbox" name="parking">
+		</li>
+		<li>
+			<label for="swimming-pool">Piscina</label>
+			<input type="checkbox" name="swimming-pool">
+		</li>
+		<li>
+			<label for="breakfast">Colazione</label>
+			<input type="checkbox" name="breakfast">
+		</li>
+		<li>
+			<label for="view">Camera con vista</label>
+			<input type="checkbox" name="view">
+		</li>
+	</ul>
+</div>
+
+
 </div>
 </div>
 
