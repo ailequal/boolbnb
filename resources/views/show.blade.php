23a515bea7f87e1749332a4c7e82d8a29fe3ee75
Stefano Riccio <riccio.stefano0@gmail.com>
2020-04-30 10:56:15 +0200

    Allineato box messaggi, fatte altre migliorie grafiche pagina flat

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -30,8 +30,8 @@
   @endphp
 
 {{-- controllo utente autenticato e se ha flat suo e se e' vuota la promo --}}
-  <div class="box-promo container">
   @if (Auth::check() && Auth::User()->id == $flats->user_id && !empty($flats->promo_service[0]) == false)
+  <div class="box-promo container">
   <div>
     <h1>Ciao {{Auth::user()->name}}, hai già pensato di mettere in risalto il tuo appartamento? </h1>
   </div>
@@ -59,35 +59,34 @@
     <input type="submit" value="Vai al pagamento" class="hidden" class="btn btn-primary" id="bottone">
   </div>
   </form>
+  </div>
   @endif
 
 
   {{-- controllo utente autenticato e se ha flat suo e se e' non e' vuota la promo --}}
   @if (Auth::check() && Auth::User()->id == $flats->user_id && !empty($flats->promo_service[0]) == true)
-
       @if ($flats->promo_service[0]->pivot->end < $now)
         <form action="{{route('payment.index')}}" method="post">
-  @csrf
-  @method('POST')
-    <span>{{$promos[0]->description}}</span>
-    <span>{{$promos[0]->price}}€</span>
-    <input type="radio" name="price" class="ciao" id="{{$promos[0]->id}}" value="{{$promos[0]->price}}">
-    
+          @csrf
+          @method('POST')
+            <span>{{$promos[0]->description}}</span>
+            <span>{{$promos[0]->price}}€</span>
+            <input type="radio" name="price" class="ciao" id="{{$promos[0]->id}}" value="{{$promos[0]->price}}">
+            
 
-    <span>{{$promos[1]->description}}</span>
-    <span>{{$promos[1]->price}}€</span>
-    <input type="radio" name="price" class="ciao" id="{{$promos[1]->id}}" value="{{$promos[1]->price}}">
+            <span>{{$promos[1]->description}}</span>
+            <span>{{$promos[1]->price}}€</span>
+            <input type="radio" name="price" class="ciao" id="{{$promos[1]->id}}" value="{{$promos[1]->price}}">
 
-    <span>{{$promos[2]->description}}</span>
-    <span>{{$promos[2]->price}}€</span>
-    <input type="radio" name="price" class="ciao" id="{{$promos[2]->id}}" value="{{$promos[2]->price}}"><br>
-    <input type="hidden" name="flat_id" value="{{$flats->id}}">
-    <input type="submit" value="Vai al pagamento" class="hidden" id="bottone">
-  </form>
+            <span>{{$promos[2]->description}}</span>
+            <span>{{$promos[2]->price}}€</span>
+            <input type="radio" name="price" class="ciao" id="{{$promos[2]->id}}" value="{{$promos[2]->price}}"><br>
+            <input type="hidden" name="flat_id" value="{{$flats->id}}">
+            <input type="submit" value="Vai al pagamento" class="hidden" id="bottone">
+        </form>
       @endif
-
   @endif
-</div>
+
 
 {{-- container main --}}
 <div class="box-desc container ">
@@ -129,6 +128,7 @@
   <div class="box-extraService">
     <div class="extra-service">
         <h2 class="extra-title">Servizi disponibili</h2>
+        {{-- ciclo per checkare servizi extra disponibili --}}
         @foreach ($flats->extra_service as $key => $extra) 
           <div class='extra-box'>
            <h2>@if($extra->name == 'wifi')
@@ -147,8 +147,6 @@
             <p>In questo appartamento è possibile fare quello scritto sopra</p>
           </div>
         @endforeach
-
-        
     </div>
   </div>
 </div>
@@ -163,17 +161,19 @@
         @method('POST')
         <h2>Scrivi al proprietario</h2>
         <div class="form-group">
-          <label class='label' for="title">Titolo</label>
-          <input class='input' type="text" name='title' placeholder="Titolo" value="{{old('title')}}">
+          <span class="label"><label class='label' for="title">Titolo</label></span>
+          <span><input class='input' type="text" name='title' placeholder="Titolo" value="{{old('title')}}"></span>
         </div>
         <div class="form-group">
-          <label class='label' for="email">Email</label>
-          <input class='input' type="text" name='email' placeholder="Email"
-            value="{{(Auth::user()) ? Auth::user()->email : old('email')}}">
+          <span class="label"><label class='label' for="email">Email</label></span>
+          <span><input class='input' type="text" name='email' placeholder="Email"
+            value="{{(Auth::user()) ? Auth::user()->email : old('email')}}"></span>
         </div>
         <div class="form-group">
-          <label class='label' for="message">Messaggio</label>
-          <input class='input' type="text" name='message' placeholder="Messaggio" value="{{old('message')}}">
+          <span class="label"><label class='label' for="message">Messaggio</label></span>
+          
+          <span><input class='input' type="text" name='message' placeholder="Messaggio" value="{{old('message')}}"></span>
+          
         </div>
         <input type="hidden" name="id" value="{{$flats->id}}"> <input class='submit' type="submit" value="Submit">
       </form>
