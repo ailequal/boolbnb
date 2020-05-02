e9c7510d951c318715bf04741134de302f6a5db5
Stefano Riccio <riccio.stefano0@gmail.com>
2020-05-02 13:36:38 +0200

    Iniziata edit

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -16,120 +16,147 @@
     @csrf
     @method('PATCH')
 
-    <div class="form-group">
-        <label for="title">Titolo</label>
-        <input type="text" name="title" placeholder="Titolo" value="{{$flat->title}}">
+    <div class="container cont-edit">
+        <h2>Modifica il tuo indirizzo</h2>
+        <div class= "row">
+            <div class="col-lg-6 col-md-12">
+                <div class="row">
+                    <div class="col-lg-6 col-md-12">
+                        <div class="form-group form-box">
+                            <label for="title">Titolo</label>
+                            <input type="text" name="title" placeholder="Titolo" value="{{$flat->title}}">
+                        </div>
+                    
+                        <div class="form-group form-box">
+                            <label for="rooms">Numero stanza</label>
+                            <input class="number" type="number" name="rooms" placeholder="Numero stanza" value="{{$flat->rooms}}">
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-12">
+                        <div class="form-group form-box">
+                            <label for="mq">Metri quadri</label>
+                            <input class="number" type="number" name="mq" placeholder="Metri quadri" value="{{$flat->mq}}">
+                        </div>
+                    
+                        <div class="form-group form-box">
+                            <label for="street">Via</label>
+                            <input type="text" name="street" placeholder="Via" value="{{$flat->flat_address->street}}">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class= "col-lg-6 col-md-12">
+                <div class="row">
+                    <div class= "col-lg-6 col-md-12">
+                        <div class="form-group form-box">
+                            <label for="street_number">Civico</label>
+                            <input class="number" type="text" name="street_number" placeholder="Civico" value="{{$flat->flat_address->street_number}}">
+                        </div>
+                        <div class="form-group form-box">
+                            <label for="city">Città</label>
+                            <input type="text" name="city" placeholder="Città" value="{{$flat->flat_address->city}}">
+                        </div>
+                    </div>
+                    <div class= "col-lg-6 col-md-12">
+                        <div class="form-group form-box">
+                            <label for="zip_code">Cap</label>
+                            <input class="number" type="text" name="zip_code" placeholder="Cap" value="{{$flat->flat_address->zip_code}}"> 
+                        </div>
+                    
+                        <div class="form-group form-box insert-img">
+                            <span class= "label">
+                                <label class= "cover-custom" for="cover">
+                                    <input id="cover" type="file" name='cover' accept='image/*'>
+                                    Scegli l'immagine di copertina
+                                </label>
+                                
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-
-    <div class="form-group">
-        <label for="rooms">Numero stanza</label>
-        <input type="number" name="rooms" placeholder="Numero stanza" value="{{$flat->rooms}}">
-    </div>
-
-    <div class="form-group">
-        <label for="mq">Metri quadri</label>
-        <input type="number" name="mq" placeholder="Metri quadri" value="{{$flat->mq}}">
-    </div>
-
-    <div class="form-group">
-        <label for="street">Via</label>
-        <input type="text" name="street" placeholder="Via" value="{{$flat->flat_address->street}}">
-    </div>
-    <div class="form-group">
-        <label for="street_number">Civico</label>
-        <input type="text" name="street_number" placeholder="Civico" value="{{$flat->flat_address->street_number}}">
-    </div>
-    <div class="form-group">
-        <label for="city">Città</label>
-        <input type="text" name="city" placeholder="Città" value="{{$flat->flat_address->city}}">
-    </div>
-    <div class="form-group">
-        <label for="zip_code">Cap</label>
-        <input type="text" name="zip_code" placeholder="Cap" value="{{$flat->flat_address->zip_code}}"> 
-    </div>
-
-    <div class="form-group">
-        <label for="cover">Modifica immagine copertina</label>
-        <input type="file" name='cover' accept='image/*' value="{{$flat->cover}}">
-    </div>
-
-    <div class="form-group">
-        <label for="guest">Numero ospiti</label>
-        <input type="text" name="guest" placeholder="Numero ospiti" value="{{$flat->guest}}">
-    </div>
-
-    <div class="form-group">
-        <label for="description">Descrizione</label>
-        <input type="text" name="description" placeholder="Descrizione" value="{{$flat->description}}">
-    </div>
-
-    <div class="form-group">
-        <label for="price_day">Prezzo giornaliero</label>
-        <input type="text" name="price_day" placeholder="Prezzo giornaliero" value="{{$flat->price_day}}">
-    </div>
-
-    <div class="form-group">
-        <label for="bathrooms">Numero bagni</label>
-        <input type="number" name="bathrooms" placeholder="Numero bagni" value="{{$flat->bathrooms}}">
-    </div>
-
-    <div class="form-group">
-        <label for="beds">Numero di Letti</label>
-        <input type="number" name="beds" placeholder="Numero letti" value="{{$flat->beds}}">
-    </div>
-    
-
-    {{-- Lat e Long mancano --}}
-
-    <div class="form-group">
-        <label for="hidden">Nascosto</label>
+   <div class="container cont-edit">
+       <h2>Modifica le caratteristiche dell'annuncio</h2>
+       <div class= "row">
+        <div class="col-lg-6 col-md-12">
+            <div class="form-group form-box">
+                <label for="guest">Numero ospiti</label>
+                <input class="number" type="text" name="guest" placeholder="Numero ospiti" value="{{$flat->guest}}">
+            </div>
         
-          <div>
-            <span>Yes</span>
-            <input type="radio" name="hidden"value="1" {{$flat->hidden == 1 ? 'checked' : ''}}>
+            <div class="form-group form-box">
+                <label for="description">Descrizione</label>
+                <input type="text" name="description" placeholder="Descrizione" value="{{$flat->description}}">
+            </div>
+        
+            <div class="form-group form-box">
+                <label for="price_day">Prezzo giornaliero</label>
+                <input class="number" type="text" name="price_day" placeholder="Prezzo giornaliero" value="{{$flat->price_day}}">
+            </div>
+        
+          
+        
+        </div>
+        <div class="col-lg-6 col-md-12">
+            <div class="form-group form-box">
+                <label for="bathrooms">Numero bagni</label>
+                <input class="number" type="number" name="bathrooms" placeholder="Numero bagni" value="{{$flat->bathrooms}}">
+            </div>
+
+            <div class="form-group form-box">
+                <label for="beds">Numero di Letti</label>
+                <input class="number" type="number" name="beds" placeholder="Numero letti" value="{{$flat->beds}}">
+            </div>
+          
+        </div>
+       </div>
+   </div>
+   <div class="container cont-edit">
+       <h2>Modifica i Servizi</h2>
+       <div class="row">
+        <div class="col-lg-6 col-md-12">
+            <div class="form-group form-box">
+                <label for="hidden">Nascosto</label>
+                <div>
+                    <span>Yes</span>
+                    <input type="radio" name="hidden"value="1" {{$flat->hidden == 1 ? 'checked' : ''}}>
+                </div>
+                <div>
+                    <span>No</span>
+                    <input type="radio" name="hidden"value="0" {{$flat->hidden == 0 ? 'checked' : ''}}>
+              </div>
           </div>
-          <div>
-            <span>No</span>
-            <input type="radio" name="hidden"value="0" {{$flat->hidden == 0 ? 'checked' : ''}}>
-          </div>
+            </div>
+           <div class="col-lg-6 col-md-12 extra-edit">
+            <h3>Servizi extra</h3>
+
+            <div class="form-group form-box extra-service-container-edit">
+                <div class = "row">
+                    @foreach ($extra_services as $extra_service)
+                    <div class="extra_services_box_edit">
+                            <span>@if($extra_service->name == 'wifi')
+                                <span><i class="fas fa-wifi"></i></span>
+                                @elseif($extra_service->name == 'smoking')
+                                <span><i class="fas fa-smoking"></i></span>
+                                @elseif($extra_service->name == 'parking')
+                                <span><i class="fas fa-parking"></i></span>
+                                @elseif($extra_service->name == 'swimming_pool')
+                                <span><i class="fas fa-swimming-pool"></i></span>
+                                @elseif($extra_service->name == 'breakfast')
+                                <span><i class="fas fa-coffee"></i></span>
+                                @elseif($extra_service->name == 'view')
+                                <span><i class="fas fa-mountain"></i></span>
+                                @endif{{$extra_service->name}}</span>
+                            <span class="checkbox"><input type="checkbox" name="extra_service[]"value="{{$extra_service->id}}"></span>
+                        </div>
+                    </div>
+                    @endforeach
+                </div>
+       </div>
+   </div>
       </div>
-
-    <h3>Servizi extra</h3>
-
-    <div class="form-group">
-        <label for="extra_service">Servizi Aggiuntivi</label>
-        @foreach ($extra_services as $extra_service)
-          <div>
-            <span>{{$extra_service->name}}</span>
-            <input type="checkbox" name="extra_service[]"value="{{$extra_service->id}}" {{($flat->extra_service->contains($extra_service->id)) ? 'checked' : ''}}>
-          </div>
-        @endforeach
-
-      </div>
-
-      {{-- <h3>Promo attivabili</h3>
-
-      <div class="form-group">
-        <label for="promo_service">Promozioni Aggiuntivi</label>
-        @foreach ($promo_services as $promo_service)
-          <div>
-            <span>{{$promo_service->description}}</span>
-            <input type="radio" name="promo_service[]"value="{{$promo_service->id}}" {{($flat->promo_service->contains($promo_service->id)) ? 'checked' : ''}}>
-          </div>
-        @endforeach --}}
-
-      </div>
-    {{-- <span>{{$tag->name}}</span>
-    <input type="checkbox" name="tags[]" value="{{$tag->id}}" @if(is_array(old('tags')) && in_array($tag->id,
-    old('tags'))) checked @endif> --}}
-
-    {{-- 'wifi',
-     'smoking',
-     'parking',
-     'swimming pool',
-     'breakfast',
-     'view' --}}
-
     <input type="submit" value="Submit">
 </form>
 @endsection
