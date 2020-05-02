4882f364a0acd5160d2fae300d5401b59f19e915
Manuel <manueldipilla3@gmail.com>
2020-05-02 12:28:20 +0200

    aggiunta modale nel payment

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
+@include('partials.successpay')
 
 <head>
     <meta charset="utf-8">
@@ -8,6 +9,7 @@
     <title>Braintree-Demo</title>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://js.braintreegateway.com/web/dropin/1.8.1/js/dropin.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
@@ -33,7 +35,7 @@
                     console.log();
                     $.get('{{ route('payment.make', $price)}}', { payload }, function (response) {
                         if (response.success) {
-                            alert('Payment successfull!');
+                            $("#exampleModal").modal("show");
                             var flatId = $('#flat_id').val();
                             var promoId = $('#promo_id').val();
                             $.ajax({
