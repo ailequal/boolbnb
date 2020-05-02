2b4e028754ba53067376c5b761cb84113fa0d8b7
Manuel <manueldipilla3@gmail.com>
2020-05-02 17:12:37 +0200

    rifixato register

--- resources/views/partials/register.blade.php
+++ resources/views/partials/register.blade.php
@@ -84,7 +84,7 @@ $(function () {
             },
             url: "{{ route('register') }}",
             data: formData,
-            success: () => window.location.assign("{{ route('account.user.index') }}"),
+            success: () => window.location.assign("{{ route('account.index') }}"),
             error: (response) => {
                 if(response.status === 422) {
                     let errors = response.responseJSON.errors;
